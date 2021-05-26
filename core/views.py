c5ee249ac3ef63c8427833087242eaa08f29bdcb
Kaium Amanbaev <kaiumamanbaev@gmail.com>
2021-05-27 02:05:03 +0600

    added interval checking in urls page

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core import serializers 
 
 from .models import Url
 
@@ -21,10 +22,18 @@ class SignOutView(LogoutView):
 
 
 class UrlsListView(LoginRequiredMixin, ListView):
+    template_name = "core/urls.html"
+    
     def get_queryset(self):
         return Url.objects.filter(user=self.request.user)
     
-    template_name = "core/urls.html"
-    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        json_serializer = serializers.get_serializer("json")()
+        
+        context["urls"] = json_serializer.serialize(
+            self.get_queryset().filter(is_paused=False),
+            ensure_ascii=False
+        )
 
-    
+        return context
