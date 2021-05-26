efc1a8e9557a57f3251bae36475537eba8ea439b
Kaium Amanbaev <kaiumamanbaev@gmail.com>
2021-05-26 23:50:31 +0600

    added url model in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Url
 
-# Register your models here.
+@admin.register(Url)
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ["id", "url", "user", "interval",
+                    "is_paused", "created_at", "updated_at"]
+    list_editable = ["url", "interval", "is_paused"]
+    
+    search_fields = ["user__username", "user__first_name",
+                     "user__last_name", "user__email", "url"]
+    
+    list_filter = ["is_paused", "created_at", "updated_at"]
+
+
+
+
+
--- core/models.py
+++ core/models.py
@@ -21,5 +21,8 @@ class Url(models.Model):
 
     is_paused = models.BooleanField(default=False)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.url
