0dc0de6a815eadca52c562d8336db8d9873d89d4
Kaium Amanbaev <kaiumamanbaev@gmail.com>
2021-05-26 23:40:54 +0600

    added Url model

---
+++ core/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+class Url(models.Model):
+    url = models.CharField(max_length=500)
+    
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True, 
+        blank=False,
+    )
+
+    interval = models.IntegerField(
+        default=10,
+        help_text="Interval of checking in seconds"
+    )
+
+    is_paused = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.url
