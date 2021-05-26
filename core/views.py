589f0d532b58f5d72d3bd4aea7ca3cc270c4877c
Kaium Amanbaev <kaiumamanbaev@gmail.com>
2021-05-27 00:22:28 +0600

    added authentication views and templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "core/homepage.html"
+
+
+class SignInView(LoginView):
+    template_name = "auth/login.html"
+
+
+class SignOutView(LogoutView):
+    next_page = "/"
+    
